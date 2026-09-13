752d27d582fdca26bf851bec82da7d5bb0395ee3
Saurabh Maurya <srbmaury@gmail.com>
2026-09-14 03:57:35 +0530

    test: restore normal service import sorting

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,9 +1,7 @@
-# ruff: noqa: I001
-
 from pathlib import Path
 
-from chess_ml_coach.services import answer_to_uci, run_puzzles, training_db_path
 from chess_ml_coach.config import Settings
+from chess_ml_coach.services import answer_to_uci, run_puzzles, training_db_path
 
 
 START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
