fa74a41ceb3d1a0e3a11faf072e35af9a01637d4
Saurabh Maurya <srbmaury@gmail.com>
2026-09-14 03:59:15 +0530

    test: normalize service test imports

--- tests/test_services.py
+++ tests/test_services.py
@@ -3,7 +3,6 @@ from pathlib import Path
 from chess_ml_coach.config import Settings
 from chess_ml_coach.services import answer_to_uci, run_puzzles, training_db_path
 
-
 START_FEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 
 
