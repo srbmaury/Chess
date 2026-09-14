3e38a744636abcb7161d9262955986243eb8d379
Saurabh Maurya <srbmaury@gmail.com>
2026-09-14 06:56:44 +0530

    fix: hide app until a player is active

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,8 +5,13 @@ import LegacyApp from './LegacyApp'
 
 export default function App() {
   const [profileRevision, setProfileRevision] = useState(0)
+  const [activeProfile, setActiveProfile] = useState<string | null | undefined>(undefined)
+
   return <>
-    <CommunityControls onProfileChanged={() => setProfileRevision((value) => value + 1)} />
-    <LegacyApp key={profileRevision} />
+    <CommunityControls
+      onProfileChanged={() => setProfileRevision((value) => value + 1)}
+      onActiveProfileChanged={setActiveProfile}
+    />
+    {activeProfile ? <LegacyApp key={`${activeProfile}-${profileRevision}`} /> : null}
   </>
 }
